6a7105bbb9069fa083453185d95d92456c8ff8d8
muskankhuranamru <muskankhurana258@gmail.com>
2024-07-19 03:22:02 +0530

    db updated from local to freemysql

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,    
-    user: process.env.DB_USER,    
-    password: process.env.DB_PASSWORD,   
-    database: process.env.DB_NAME 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306 
 });
 
 connection.connect((err) => {
